241e7323f8c96df8bca7ddf41b8b3ac1a48a5ab1
squidbus <1249084-squidbus@users.noreply.gitlab.freedesktop.org>
2026-05-29 17:48:20 -0700

    kk: Support VK_EXT_external_memory_host
    Metal does not support importing host memory pointers into MTLHeap,
    only MTLBuffer. Buffers can import without issue, and images are
    restricted to linear images without flags requiring aliasing.
    
    Reviewed-by: Aitor Camacho <aitor@lunarg.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/41894>
    (cherry picked from commit 94295fda67e2acbf216b57b29660d5c1c962ab68)

--- src/kosmickrisp/vulkan/kk_physical_device.c
+++ src/kosmickrisp/vulkan/kk_physical_device.c
@@ -163,6 +163,7 @@ kk_get_device_extensions(const struct kk_instance *instance,
       .EXT_depth_clip_control = true,
       .EXT_extended_dynamic_state3 = true,
       .EXT_external_memory_metal = true,
+      .EXT_external_memory_host = true,
       .EXT_image_2d_view_of_3d = true,
       .EXT_load_store_op_none = true,
       .EXT_memory_budget = true,
@@ -692,6 +693,9 @@ kk_get_device_properties(const struct kk_physical_device *pdev,
 
       /* VK_EXT_extended_dynamic_state3 */
       .dynamicPrimitiveTopologyUnrestricted = false,
+
+      /* VK_EXT_external_memory_metal */
+      .minImportedHostPointerAlignment = os_page_size,
 
       /* VK_EXT_graphics_pipeline_library */
       .graphicsPipelineLibraryFastLinking = true,
