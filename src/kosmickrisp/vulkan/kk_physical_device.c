a89495f3ea3e2d0abda953c01acc62e6f260598c
squidbus <1249084-squidbus@users.noreply.gitlab.freedesktop.org>
2026-06-03 05:04:06 -0700

    kk: Advertise additional tessellation dynamic state
    These are already supported by the tessellation implementation.
    
    Reviewed-by: Aitor Camacho <aitor@lunarg.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/42016>
    (cherry picked from commit 1e08ccf28d421b9796e009eb969d78420938b97f)

--- src/kosmickrisp/vulkan/kk_physical_device.c
+++ src/kosmickrisp/vulkan/kk_physical_device.c
@@ -297,7 +297,7 @@ kk_get_device_features(
       .extendedDynamicState = true,
       .extendedDynamicState2 = true,
       .extendedDynamicState2LogicOp = false,
-      .extendedDynamicState2PatchControlPoints = false,
+      .extendedDynamicState2PatchControlPoints = true,
       .inlineUniformBlock = true,
       .maintenance4 = true,
       .pipelineCreationCacheControl = true,
@@ -402,6 +402,7 @@ kk_get_device_features(
       .extendedDynamicState3DepthClampEnable = true,
       .extendedDynamicState3DepthClipNegativeOneToOne = true,
       .extendedDynamicState3LineRasterizationMode = true,
+      .extendedDynamicState3TessellationDomainOrigin = true,
 
       /* EXT_image_2d_view_of_3d */
       .image2DViewOf3D = true,
