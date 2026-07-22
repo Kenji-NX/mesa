4c9dfab88993bd22195035cf01ba7ce82c3c68e1
squidbus <1249084-squidbus@users.noreply.gitlab.freedesktop.org>
2026-07-22 04:05:22 -0700

    kk: Support VK_EXT_provoking_vertex
    This is needed to be able to properly emulate OpenGL and other GPUs
    which support changing the provoking vertex.
    
    Unrolling is applied to re-write the index buffer, putting the last
    vertex first while preserving winding order.
    
    The performance impact is minimized by only applying unrolling under
    the following conditions:
    
     * The user has configured `VK_PROVOKING_VERTEX_MODE_LAST_VERTEX_EXT`
     * The active fragment shader uses flat inputs
    
    This means that unrolling will never apply if the user does not
    change the provoking vertex, and for use cases like OpenGL emulation
    which default to last vertex, unrolling will never happen unless the
    shader actually needs it.
    
    Reviewed-by: Aitor Camacho <aitor@lunarg.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/43143>
    (cherry picked from commit 1388ec1b72fb926a5c83401535e84b8ba95c2e0a)

--- src/kosmickrisp/vulkan/kk_physical_device.c
+++ src/kosmickrisp/vulkan/kk_physical_device.c
@@ -187,6 +187,7 @@ kk_get_device_extensions(const struct kk_instance *instance,
       .EXT_post_depth_coverage = true,
       .EXT_primitive_restart_index = true,
       .EXT_primitive_topology_list_restart = true,
+      .EXT_provoking_vertex = true,
       .EXT_robustness2 = true,
       .EXT_sample_locations = true,
       .EXT_shader_atomic_float = true,
@@ -451,6 +452,7 @@ kk_get_device_features(
       .extendedDynamicState3DepthClampEnable = true,
       .extendedDynamicState3DepthClipNegativeOneToOne = true,
       .extendedDynamicState3LineRasterizationMode = true,
+      .extendedDynamicState3ProvokingVertexMode = true,
       .extendedDynamicState3SampleLocationsEnable = true,
       .extendedDynamicState3TessellationDomainOrigin = true,
 
@@ -477,6 +479,9 @@ kk_get_device_features(
       /* VK_EXT_primitive_topology_list_restart */
       .primitiveTopologyListRestart = true,
       .primitiveTopologyPatchListRestart = false,
+
+      /* VK_EXT_provoking_vertex */
+      .provokingVertexLast = true,
 
       /* VK_EXT_shader_replicated_composites */
       .shaderReplicatedComposites = true,
